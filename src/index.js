f8bb9584edaab67dc11a36064522b68b8e5199be
nirdeshtiwari432 <nirdesh26cs081@satiengg.in>
2025-12-25 11:15:57 +0530

    cors setup

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://fitnessbackend-5s8g.onrender.com",
+    origin: "https://fitnessbyai-j6ee.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
